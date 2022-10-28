4f31dc61728d7e008b952b0dbf75154745abda35
tukumo0411 <pianosonata0308@icloud.com>
2022-10-28 14:45:44 +0900

    言語・都道府県

--- database/seeds/DevelopmentLangagesTableSeeder.php
+++ database/seeds/DevelopmentLangagesTableSeeder.php
@@ -11,6 +11,90 @@ class DevelopmentLangagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('language')->insert([
+            [
+                'language_name' => 'Apex'
+            ],
+
+            [
+                'language_name' => 'c'
+            ],
+
+            [
+                'language_name' => 'c#'
+            ],
+
+            [
+                'language_name' => 'c++'
+            ],
+
+            [
+                'language_name' => 'Dart'
+            ],
+
+            [
+                'language_name' => 'ElixirGo'
+            ],
+
+            [
+                'language_name' => 'Java'
+            ],
+
+            [
+                'language_name' => 'JavaScript'
+            ],
+
+            [
+                'language_name' => 'Kotlin'
+            ],
+
+            [
+                'language_name' => 'Objective'
+            ],
+
+            [
+                'language_name' => 'Peal'
+            ],
+
+            [
+                'language_name' => 'php'
+            ],
+
+            [
+                'language_name' => 'Python'
+            ],
+
+            [
+                'language_name' => 'R'
+            ],
+
+            [
+                'language_name' => 'Ruby'
+            ],
+
+            [
+                'language_name' => 'Rust'
+            ],
+
+            [
+                'language_name' => 'Scala'
+            ],
+
+            [
+                'language_name' => 'Swift'
+            ],
+
+            [
+                'language_name' => 'TypeScript'
+            ],
+
+            [
+                'language_name' => '言語不当'
+            ],
+
+            [
+                'language_name' => 'その他'
+            ],
+        ]);
     }
 }
