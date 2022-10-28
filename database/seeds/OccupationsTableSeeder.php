4e40cb59f148025914d7358074eb022f1d80d60b
funyanko <ooi2172406@stu.o-hara.ac.jp>
2022-10-28 14:43:14 +0900

    あらかた

--- database/seeds/OccupationsTableSeeder.php
+++ database/seeds/OccupationsTableSeeder.php
@@ -11,6 +11,85 @@ class OccupationsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('ranks')->insert([
+            [
+                'occupation_name' => 'フロントエンドエンジニア',
+            ],
+            [
+                'occupation_name' => 'バックエンドエンジニア',
+            ],
+            [
+                'occupation_name' => 'インフラエンジニア',
+            ],
+            [
+                'occupation_name' => 'セキュリティエンジニア',
+            ],
+            [
+                'occupation_name' => 'ネットワークエンジニア',
+            ],
+            [
+                'occupation_name' => 'データベースエンジニア',
+            ],
+            [
+                'occupation_name' => 'フルスタックエンジニア',
+            ],
+            [
+                'occupation_name' => 'DevOpsエンジニア',
+            ],
+            [
+                'occupation_name' => '機械学習エンジニア',
+            ],
+            [
+                'occupation_name' => 'iosエンジニア',
+            ],
+            [
+                'occupation_name' => 'Androidエンジニア',
+            ],
+            [
+                'occupation_name' => 'QAエンジニア',
+            ],
+            [
+                'occupation_name' => 'UXエンジニア',
+            ],
+            [
+                'occupation_name' => 'データサイエンティスト',
+            ],
+            [
+                'occupation_name' => '組み込みエンジニア',
+            ],
+            [
+                'occupation_name' => 'ゲーム開発エンジニア',
+            ],
+            [
+                'occupation_name' => 'SIエンジニア',
+            ],
+            [
+                'occupation_name' => '情報システム',
+            ],
+            [
+                'occupation_name' => 'プリセールス',
+            ],
+            [
+                'occupation_name' => 'テクニカルサポートエンジニア',
+            ],
+            [
+                'occupation_name' => 'エンジニアマネージャー,
+            ],
+            [
+                'occupation_name' => 'プロダクトマネージャー',
+            ],
+            [
+                'occupation_name' => '技術顧問',
+            ],
+            [
+                'occupation_name' => 'マークアップエンジニア',
+            ],
+            [
+                'occupation_name' => 'プロジェクトマネージャー',
+            ],
+            [
+                'occupation_name' => 'データエンジニアios/Android',
+            ],
+        ]);
     }
 }
