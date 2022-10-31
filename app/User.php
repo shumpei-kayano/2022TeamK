90bd34185aebd9a99d782097bb67808fdb656591
ishikawakaito <k.ishikawa0323@icloud.com>
2022-10-31 14:08:00 +0900

    メール送れた

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\Events\Registered;
 
+
 class User extends Authenticatable  implements MustVerifyEmail
 {
     use Notifiable;
