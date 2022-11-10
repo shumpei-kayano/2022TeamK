1e19dc6a290b126739e3f3aa8cd7a43f76441b17
ishikawakaito <k.ishikawa0323@icloud.com>
2022-11-10 11:14:40 +0900

    11/10河野雄介バカ

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -57,7 +57,7 @@ class PortfolioController extends Controller
         // $portfolio->save();
         // // return redirect('portfolio',compact('languages'));
         // return view('./portfolio/portfolio', );
-        $this->middleware('auth', ['except' => ['user_id', 'update_at', 'created_at']]);
+        // $this->middleware('auth', ['except' => ['user_id', 'update_at', 'created_at']]);
         $portfolio = new Portfolio;
         $form = $request->all();
 
@@ -69,9 +69,10 @@ class PortfolioController extends Controller
 
     public function edit(Request $request)
     {
-        $items = \DB::table('development_languages') -> get();
-        $portfolio = Portfolio::find($request->id);
         $user = Auth::user();
+        $portfolio = Portfolio::whereUser_id($user->id)->first();
+        $items = \DB::table('development_languages') -> get();
+        
         return view('./portfolio/portfolioEdit',['form' => $portfolio] ,compact('items','user'));
     }
 
@@ -82,28 +83,29 @@ class PortfolioController extends Controller
         // $portfolio = Portfolio::find($id);
         // dd($portfolio);
 
-        $portfolio = Portfolio::find($request -> id);
-        $portfolio->name = $request->name;
-        $portfolio->email = $request->email;
-        $portfolio->tel = $request->tel;
-        $portfolio->educational_background = $request->educational_background;
-        $portfolio->development_language_id1 = $request->development_language_id1;
-        $portfolio->development_year1 = $request->development_year1;
-        $portfolio->development_language_id2 = $request->development_language_id2;
-        $portfolio->development_year2 = $request->development_year2;
-        $portfolio->development_language_id3 = $request->development_language_id3;
-        $portfolio->development_year3 = $request->development_year3;
-        $portfolio->development_language_id4 = $request->development_language_id4;
-        $portfolio->development_year4 = $request->development_year4;
-        $portfolio->development_language_id5 = $request->development_language_id5;
-        $portfolio->development_year5 = $request->development_year5;
-        $portfolio->self_pr = $request->self_pr;
-        $portfolio->birthday = $request->birthday;
-        $portfolio->save();
+        // $portfolio = Portfolio::find($request -> id);
+        // $portfolio->name = $request->name;
+        // $portfolio->email = $request->email;
+        // $portfolio->tel = $request->tel;
+        // $portfolio->educational_background = $request->educational_background;
+        // $portfolio->development_language_id1 = $request->development_language_id1;
+        // $portfolio->development_year1 = $request->development_year1;
+        // $portfolio->development_language_id2 = $request->development_language_id2;
+        // $portfolio->development_year2 = $request->development_year2;
+        // $portfolio->development_language_id3 = $request->development_language_id3;
+        // $portfolio->development_year3 = $request->development_year3;
+        // $portfolio->development_language_id4 = $request->development_language_id4;
+        // $portfolio->development_year4 = $request->development_year4;
+        // $portfolio->development_language_id5 = $request->development_language_id5;
+        // $portfolio->development_year5 = $request->development_year5;
+        // $portfolio->self_pr = $request->self_pr;
+        // $portfolio->birthday = $request->birthday;
+        // $portfolio->save();
         // $languages = \DB::table('development_languages') -> get();
         
-        $portfolio = new Portfolio;
+        $portfolio = Portfolio::find($request -> user_id);
         $form = $request->all();
+
         unset($form['_token']);
         $portfolio->fill($form)->save();
         return view('./portfolio/portfolio');
