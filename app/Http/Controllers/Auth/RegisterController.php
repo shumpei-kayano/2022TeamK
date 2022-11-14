1b60b128947fd184b5b245b413338df3440c1d22
ishikawakaito <k.ishikawa0323@icloud.com>
2022-11-14 15:12:35 +0900

    11/14

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,12 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+
         return User::create([
-            'check' => $data['check'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'check' => $data['check'],
         ]);
-        dd('check');
     }
 }
