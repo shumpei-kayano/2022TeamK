71157217daef3b742a05247be21931910c5e8c41
easttiger1230 <easttiger.1230@au.com>
2022-11-15 16:39:13 +0900

    ノスケ

--- resources/views/userMypage/accountEdit.blade.php
+++ resources/views/userMypage/accountEdit.blade.php
@@ -7,18 +7,16 @@
 @section('content')
 
 <h1 class="p-info">アカウントじょうほうへんしゅう</h1>
-<div class="p-acinfo__container">
-    <div class="nes-container is-rounded is-dark p-acinfo">
-        <p class="p-acinfo__idEdit">        
-            ユーザーID:<input type="text" name='name' value='{{ $form->name }}'><br>
-        </p>
-            メールアドレス:<input type="text" name='email'value='{{ $form->email }}'>
+<div class="p-acinfoEdit__container">
+    <div class="nes-container is-rounded is-dark p-acinfoEdit">
+            ユーザーID<input class="nes-input is-dark p-acinfoEdit__input" type="text" name='name' value='{{ $form->name }}'>
+            メールアドレス<input class="nes-input is-dark p-acinfoEdit__input" type="text" name='email'value='{{ $form->email }}'>
     </div>
 </div>
 @csrf<form action="{{ route('account_update') }}" method="post">
 
-<div class="p-acinfo__btn-container">
-    <button type="submit" class="nes-btn is-success p-acinfo__btn">決定
+<div class="p-acinfoEdit__btn-container">
+    <button type="submit" class="nes-btn is-success p-acinfoEdit__btn">決定
     </button>
 </div>
 @endsection
