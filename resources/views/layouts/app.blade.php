a7b6c72bf06c6bdb9326a6cfce568e9fa9231a2b
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2022-11-16 11:21:26 +0900

    ログインページ作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,9 +50,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                            {{--  <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('サインイン') }}</a>
-                            </li>
+                            </li>  --}}
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('サインアップ') }}</a>
