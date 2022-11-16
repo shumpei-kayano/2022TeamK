d21584a8d1acae11d827b51a3dc6d2ec09c0de63
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2022-11-16 14:24:42 +0900

    portfolioの作成

--- resources/views/portfolio/portfolio.blade.php
+++ resources/views/portfolio/portfolio.blade.php
@@ -1,19 +1,13 @@
 @extends('layouts.app')
 
+@section('title','ポートフォリオ')
+
 @section('content')
-    
-    <form method="GET" action="portfolioAdd">
-        @csrf
-        <input type="submit" name="portfolioAdd" value="ポートフォリオ作成">
-    </form>
-
-    <form method="GET" action="portfolioEdit">
-        @csrf
-        <input type="submit" name="portfolioUpdate" value="ポートフォリオ編集">
-    </form>
-
-    <form method="GET" action="{{ route('portfolioDel') }}">
-        @csrf
-        <input type="submit" name="portfolioDelete" value="ポートフォリオ削除">
-    </form>
+<div class="portfolio">
+    <div class="nes-container is-dark with-title portfolio__container">
+        <a href="portfolioAdd"><button type="button" class="nes-btn is-primary portfolio__btn">ポートフォリオを<br>つくる</button></a>
+        <a href="portfolioEdit"><button type="button" class="nes-btn is-warning portfolio__btn">ポートフォリオを<br>へんしゅうする</button></a>
+        <a href="portfolioDel"><button type="button" class="nes-btn is-error portfolio__btn">ポートフォリオを<br>さくじょする</button></a>
+    </div>
+</div>
 @endsection
