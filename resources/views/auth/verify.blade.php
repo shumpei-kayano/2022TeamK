6326689e920c9052ca674fda94a157eefed4632e
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2022-11-16 12:42:02 +0900

    verify画面の作成

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,27 +1,26 @@
 @extends('layouts.app')
 
+@section('title','アドレス認証エラー')
+    
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="verify">
+    <div class="nes-container is-dark with-title verify__container">
+        <p class="title">{{ __('メールアドレスのにんしょうが完了していません') }}</p>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+        <div class="card-body">
+            @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('あなたのメールアドレスにあたしいリンクを送信しました') }}
                 </div>
-            </div>
+            @endif
+
+            {{ __('次に進む前に、リンクが書かれたメールをかくにんしてください。') }}<br>
+            {{ __('メールがとどいていない場合') }}
+            <form class="verify-btn" method="POST" action="{{ route('verification.resend') }}">
+                @csrf
+                <button type="submit" class="nes-btn is-warning">{{ __('再送信はコチラ') }}</button>
+            </form>
         </div>
     </div>
 </div>
