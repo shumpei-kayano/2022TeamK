5f29b44082d35821dc0423a10ef0198eb464f200
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2022-11-16 14:05:30 +0900

    resetとconfirmの作成

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,61 +1,58 @@
 @extends('layouts.app')
 
+@section('title', 'パスワードリセット')
+
 @section('content')
 <div class="reset">
-    <div class="reset__container">
-        <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="reset__container nes-container is-dark with-title">
+        <p class="title">パスワードをリセットします</p>
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
 
-        <div class="card-body">
-            <form method="POST" action="{{ route('password.update') }}">
-                @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
 
-                <input type="hidden" name="token" value="{{ $token }}">
+            <div class="form-group row">
+                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
 
-                <div class="form-group row">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                <div class="col-md-6">
+                    <input id="email" type="email" class="nes-input is-dark @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
+            </div>
 
-                <div class="form-group row">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                <div class="col-md-6">
+                    <input placeholder="8ケタ以上" id="password" type="password" class="nes-input is-dark @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
+            </div>
 
-                <div class="form-group row">
-                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+            <div class="form-group row">
+                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('パスワード（再度）') }}</label>
 
-                    <div class="col-md-6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                    </div>
+                <div class="col-md-6">
+                    <input placeholder="もう一度" id="password-confirm" type="password" class="nes-input is-dark" name="password_confirmation" required autocomplete="new-password">
                 </div>
+            </div>
 
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Reset Password') }}
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
+            <div class="reset__btn-container">
+                <button type="submit" class="nes-btn is-warning reset__btn">
+                    {{ __('パスワードをリセット') }}
+                </button>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
