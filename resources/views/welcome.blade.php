c0bc4316e3ccc00507981d1f62a6281388456a67
easttiger1230 <easttiger.1230@au.com>
2022-11-17 12:02:41 +0900

    うぇるかむ完成（仮）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,87 +1,78 @@
 <!DOCTYPE html>
+
+@section('content')
+
+@section('title')
+マッチングRPG
+@endsection
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />    
+        {{--  ゲーム風cssのnes.css読み込み  --}}
+        <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
+        <title>@yield('title')</title>
+
+          <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
+
+    <style>
+        body {
+            background: url("/images/8bit.jpg") top center/cover;
+        }
+    </style>
+
     <body>
-        <div class="flex-center position-ref full-height">
+
+<div class="p-wel">
+    <div class="p-wel__midasi">
+        <h2>マッチングRPG<i class="nes-pokeball"></i></h2>
+    </div>
+
+        <div class="flex-center position-ref full-height p-wel__home">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                    <button type="button" class="nes-btn p-wel__btn" onclick="location.href="{{ url('/home') }}">ほーむ</button>
+                        {{-- <a href="{{ url('/home') }}">Home</a> --}}
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                    <button type="button" class="nes-btn p-wel__btn" onclick="location.href="{{ route('login') }}">ろぐいん</button>
+                        {{-- <a href="{{ route('login') }}">Login</a> --}}
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                        <button type="button" class="nes-btn p-wel__btn" onclick="location.href="{{ route('register') }}">しんきとうろく</button>
+                            {{-- <a href="{{ route('register') }}">Register</a> --}}
                         @endif
                     @endauth
                 </div>
             @endif
+        </div>
 
-            <div class="content">
+</div>
+
+
+
+
+            {{-- <div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
@@ -100,7 +91,7 @@
         </div>
         <div id="app">
             <example-component></example-component>
-        </div>
+        </div> --}}
         <script src="{{mix('js/app.js')}}"></script>
     </body>
 </html>
