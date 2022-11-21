2a64c082c2f17028f6840d98d69426ae3265a8fd
easttiger1230 <easttiger.1230@au.com>
2022-11-21 13:35:02 +0900

    原神したいなぁ

--- resources/views/portfolio/portfolioAdd.blade.php
+++ resources/views/portfolio/portfolioAdd.blade.php
@@ -7,7 +7,7 @@
 <div class="p-form">
 <h1 class="p-form">ポートフォリオ作成</h1>
 <div class="p-port__container">
-    <div class="nes-container is-rounded is-dark">
+    <div class="nes-container is-rounded is-dark" style="height: 600px;">
     <form action='{{ route('portfolio_create') }}'method='post'>
     @csrf
 
@@ -15,23 +15,32 @@
             <input type="hidden" name="user_id" value="{{ $user->id }}">
             {{-- userId <input type="int" name="user_id"> --}}
 
-            <!-- 名前 -->        
+            <!-- 名前 <br><input type="text" name='name'><br> -->        
             <label for="dark_field" style="color:#fff;" >名前<br>
             <input type="search" id="dark_field" class="nes-input is-dark p-form__portfolio" name="name"><br>
 
-            <!-- メールアドレス -->
+            <!-- メールアドレス <br><input type="text" name='email'><br> -->
             <label for="dark_field" style="color:#fff;" >メールアドレス<br>
                 <input type="search" id="dark_field" class="nes-input is-dark p-form__portfolio" name="email"><br>
 
-            <!-- 電話番号 -->
+            <!-- でんわばんごう <br><input type="text" name='tel'><br> -->
             <label for="dark_field" style="color:#fff;" >でんわばんごう<br>
                 <input type="search" id="dark_field" class="nes-input is-dark p-form__portfolio" name="tel"><br>
             
-            <!-- 最終学歴 -->
+            <!-- さいしゅうがくれき <br><input type="text" name='educational_background'><br> -->
              <label for="dark_field" style="color:#fff;" >さいしゅうがくれき<br>
                 <input type="search" id="dark_field" class="nes-input is-dark p-form__portfolio" name="educational_background"><br>
 
                 生年月日<br> <input type="date" value="yyyy/mm/dd" name="birthday"><br>
+
+
+        <div class="p-port__under">
+                    <!-- 自己PR -->
+            <div class="p-port__PR">
+                <label for="dark_field" style="color:#fff; margin-top: 20px;>自己PR</label><br>
+                    <input type="search" id="dark_field" class="nes-input is-dark p-form__PR" name="self_pr"><br>
+            </div>
+        </div>        
     </div>
 
  
@@ -39,7 +48,7 @@
 
 
         <!-- ここの下の部分は１つずつdivで囲んで横並びにさせる！！！ -->
-<div class="p-port__right">
+{{-- <div class="p-port__right"> --}}
     <div class="p-port">
 
         <div class="p-fort__aaa">
@@ -192,24 +201,19 @@
 
 
 {{----------------------------------------------------------------------------------------------------}}
-            
-
-<div class="p-port__under">
-            <!-- 自己PR -->
-        <div class="p-port__PR">
-            <label for="dark_field" style="color:#fff; margin-top: 20px;" >自己PR</label><br>
-                <input type="search" id="dark_field" class="nes-input is-dark p-form__PR" name="self_pr"><br>
-        </div>
-        </div>
-        
+{{--             
+</div>
+            </div>
         </div>
     </div>
-</div>
-        <!-- 作成ボタン -->
+</div> --}}
+
+
+<br>
         <div class="p-acinfo__btn-container p-port__btn">
         <button type="submit" class="nes-btn is-success p-acinfo__btn">さくせい</button>
         <div>
 
         </form>
-        </div> 
+        {{-- </div>  --}}
 @endsection
