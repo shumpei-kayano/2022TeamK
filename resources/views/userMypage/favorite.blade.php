83e4dc0505137314e6a4e0f03b330ba38f7cd4fe
easttiger1230 <easttiger.1230@au.com>
2022-11-24 13:34:29 +0900

    W杯

--- resources/views/userMypage/favorite.blade.php
+++ resources/views/userMypage/favorite.blade.php
@@ -4,10 +4,14 @@
 
 @section('content')
 
-<h1 class="p-info">お気に入りリスト</h1>
+<h1 class="p-form">お気に入りリスト</h1>
 
-<div>
-    それでも！
+<div class="favorite">
+    <div class="nes-container is-dark with-title favorite__container">
+        <a href="https://www.instagram.com/miura_koutaro?ref=badge" class="insta_btn3">
+            <i class="fab fa-instagram"></i><div>Follow Me</div>
+          </a>
+    </div>
 </div>
   
 
