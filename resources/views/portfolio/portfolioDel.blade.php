435865b44435361d321e33eeea844f6fb35087c2
tukumo0411 <pianosonata0308@icloud.com>
2022-11-28 14:30:21 +0900

    ポートフォリオ削除画面の見た目編集（プルダウン途中）

--- resources/views/portfolio/portfolioDel.blade.php
+++ resources/views/portfolio/portfolioDel.blade.php
@@ -4,53 +4,74 @@
 
 @section('content')
 
-<div class="p-form">
-    <h1 class="p-form">ポートフォリオ削除</h1>
-<div class="p-acinfo__container">
-<div class="nes-container is-rounded is-dark p-acinfo">
+<h1 class="p-form">ポートフォリオさくじょ</h1>
+<div class="p-port">
+    <div class="nes-container is-rounded is-dark" style="height: 600px;">
 
-    <!-- 名前 -->
-    <label for="dark_field" style="color:#fff;" >名前<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->name }}</p><br>
+        <div class="p-port__container">
+            <div class="p-port__left">
+                    <!-- 名前 -->
+                    <label for="name" style="color:#fff;" >名前<br>
+                        @if($errors->has('name'))
+                            {{ $errors->first('name') }}
+                        @endif
+                        <input type="text" id="name" class="nes-input is-dark p-form__portfolio" name="name" disabled value='{{ $form->name }}'><br>
 
-    <!-- メールアドレス -->
-    <label for="dark_field" style="color:#fff;" >メールアドレス<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->email }}</p><br>
+                    <!-- メールアドレス -->
+                    <label for="mail" style="color:#fff;" >メールアドレス<br>
+                        @if($errors->has('email'))
+                            {{ $errors->first('email') }}
+                        @endif
+                            <input type="mail" id="mail" class="nes-input is-dark p-form__portfolio" name="email" disabled value='{{ $form->email }}'><br>
 
-    <!-- 電話番号 -->
-    <label for="dark_field" style="color:#fff;" >でんわばんごう<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->tel }}</p><br>
+                    <!-- 電話番号 -->
+                    <label for="tel" style="color:#fff;" >でんわばんごう<br>
+                        @if($errors->has('tel'))
+                            {{ $errors->first('tel') }}
+                        @endif
+                            <input type="tel" id="tel" class="nes-input is-dark p-form__portfolio" name="tel" disabled value='{{ $form->tel }}'><br>
 
-    <!-- 最終学歴 -->
-    <label for="dark_field" style="color:#fff;" >さいしゅうがくれき<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->educational_background }}</p><br>
+                <!-- 最終学歴 -->
+                <label for="gakureki" style="color:#fff;" >さいしゅうがくれき<br>
+                    @if($errors->has('educational_background'))
+                        {{ $errors->first('educational_background') }}
+                    @endif
+                        <input type="text" id="gakureki" class="nes-input is-dark p-form__portfolio" name="educational_background" disabled value='{{ $form->educational_background }}'><br>
 
-    <!-- 学習言語1 -->
-    <label for="dark_field" style="color:#fff;" >がくしゅうげんご1<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->development_language->language_name }}</p><br>
+                <!-- 生年月日 -->
+                <label for="birthday" style="color:#fff;" >生年月日<br>
+                    @if($errors->has('birthday'))
+                        {{ $errors->first('birthday') }}
+                    @endif
+                        <input type="date" id="birthday" class="nes-input is-dark p-form__portfolio" disabled value="yyyy/mm/dd" name="birthday">
+             </div>
 
-    <!-- 学習期間1 -->
-    <label for="dark_field" style="color:#fff;" >がくしゅうきかん1<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->development_year1 }}</p><br>
 
-    <!-- 自己PR -->
-    <label for="dark_field" style="color:#fff;" >自己PR<br>
-        <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->self_pr }}</p><br>
+             <div class="p-port__right">
 
-    <!-- 生年月日 -->
-    <label for="dark_field" style="color:#fff;" >生年月日<br>
-        <p id="dark_field" class="nes-input is-dark p-form__portfolio" name="name" >{{ $form->birtday }}</p><br>
+                <!-- 学習言語1 -->
+                <label for="dark_field" style="color:#fff;" >がくしゅうげんご1<br>
+                    <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->development_language->language_name }}</p><br>
+
+                <!-- 学習期間1 -->
+                <label for="dark_field" style="color:#fff;" >がくしゅうきかん1<br>
+                    <p id="dark_field" class="nes-input is-dark p-portDel__port" name="name" >{{ $form->development_year1 }}</p><br>
+
+            </div>
+
+                    <!-- 自己PR -->
+                    <div class="p-port__PR">
+                        <label for="pr" style="color:#fff; margin-top: 20px;">自己PR</label><br>
+                            <textarea name="self_pr" id="" cols="27" rows="19" class="nes-textarea is-dark" readonly>{{ $form->self_pr }}</textarea>
+                    </div>
+                </div>
+
+            <!-- 削除ボタン -->
+            <form action="{{ route('portfolio_remove') }}" method="post">
+                <div class="p-acinfo__btn-container p-port__btn">
+                    @csrf
+                    <button type="submit" class="nes-btn is-success p-acinfo__btn">さくじょ</button>
+                </div>
+            </form>
 </div>
-</div>
-
-    <!-- 削除ボタン -->
-    <form action="{{ route('portfolio_remove') }}" method="post">
-        <div class="p-form">
-    @csrf
-    <input type="submit" class="nes-btn is-success" value='削除'>
-    </div>
-
-</form>
-</div>
-
 @endsection
