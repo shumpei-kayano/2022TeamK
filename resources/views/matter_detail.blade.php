e12512dab0e3176f5cfdd641c5b9826b3ff718fd
tukumo0411 <pianosonata0311@icloudo.com>
2023-01-11 12:10:25 +0900

    アカ情報の「ステータス」を抹消した

--- resources/views/matter_detail.blade.php
+++ resources/views/matter_detail.blade.php
@@ -7,54 +7,60 @@
 @section('content')
 
 <h1 class="p-form">詳細</h1>
-<div class="nes-container is-dark with-title p-mypage__container">
-<table class="">
-    <thead>
-      <tr>
-        <th>案件名</th>
-        <th>ランク</th>
-        <th>特記事項</th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{ $matter->matter_name }}</td>
-        <td>{{ $matter->rank }}</td>
-        <td>{{ $matter->remarks }}</td>
-        {{-- <td>{{ dd($favorite) }}</td> --}}
-        <td>@if (Auth::check())
-                  @if (isset($favorite))
-                  
-                      {{-- favoriteがあったら削除ボタン表示 --}}
-                      <form action="{{route('favorite.del2', ['id'=>$matter->id])}}" method="POST">
-                          <input type="hidden" name="matter_id" value="{{$matter->id}}">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit">お気に入り解除</button>
-                      </form>
-                  @else
-                      {{-- favoliteがなかったらお気に入り登録ボタン表示 --}}
-                      <form action="{{route('favorite2', ['id'=>$matter->id])}}" method="POST">
-                          @csrf
-                          <input type="hidden" name="matter_id" value="{{$matter->id}}">
-                          <button type="submit">お気に入り登録</button>
-                      </form>
-                  @endif
-          @endif
-        </td>
-        <td>
+  <div class="p-acinfo__container2">
+    <div class="nes-container is-dark with-title p-mypage__container">
 
-          {{--  応募するボタン  --}}
-          <form action="{{route('submission')}}" method="POST">
-            @csrf
-            <input type="hidden" name="matter_id" value="{{$matter->id}}">
-            <button type="submit">おうぼする</button>
-        </form>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-          </div>
+      <table class="">
+          <thead>
+            <tr>
+              <th>案件名</th>
+              <th>ランク</th>
+              <th>特記事項</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>{{ $matter->matter_name }}</td>
+              <td>{{ $matter->rank }}</td>
+              <td>{{ $matter->remarks }}</td>
+              {{-- <td>{{ dd($favorite) }}</td> --}}
+              <td>@if (Auth::check())
+                        @if (isset($favorite))
+                        
+                            {{-- favoriteがあったら削除ボタン表示 --}}
+                            <form action="{{route('favorite.del2', ['id'=>$matter->id])}}" method="POST">
+                                <input type="hidden" name="matter_id" value="{{$matter->id}}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">お気に入りかいじょ</button>
+                            </form>
+                        @else
+                            {{-- favoliteがなかったらお気に入り登録ボタン表示 --}}
+                            <form action="{{route('favorite2', ['id'=>$matter->id])}}" method="POST">
+                                @csrf
+                                <input type="hidden" name="matter_id" value="{{$matter->id}}">
+                                <button type="submit">お気に入りとうろく</button>
+                            </form>
+                        @endif
+                @endif
+              </td>
+              <td>
+
+                {{--  応募するボタン  --}}
+                <form action="{{route('submission')}}" method="POST">
+                  @csrf
+                  <input type="hidden" name="matter_id" value="{{$matter->id}}">
+                  <button type="submit">おうぼする</button>
+              </form>
+              </td>
+            </tr>
+
+          </tbody>
+      </table>
+      
+    </div>
+  </div>
 @endsection
