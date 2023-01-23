12dd1be177cecdf5d01bdc3058e9b9f5c7df810a
easttiger1230 <easttiger.1230@au.com>
2023-01-23 12:20:02 +0900

    私のたった一つの望み、可能性の獣、希望の象徴・・・父さん、母さんごめん、俺は・・・行くよ！！

--- resources/views/userMypage/favorite.blade.php
+++ resources/views/userMypage/favorite.blade.php
@@ -7,7 +7,7 @@
 <h1 class="p-form">お気に入りリスト</h1>
 
 <div class="p-acinfo__container2">
-    <div class="nes-container is-dark with-title p-form__container2">
+    <div class="nes-container is-dark with-title p-form__container2" style="width:600px; height:150px;">
         {{-- <a href="https://www.instagram.com/miura_koutaro?ref=badge" class="insta_btn3">
             <i class="fab fa-instagram"></i><div style="text-align: center">それでも!!!</div>
         </a> --}}
