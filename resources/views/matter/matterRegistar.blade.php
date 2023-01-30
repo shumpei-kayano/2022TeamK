490e8f3ce39780a5a4c1fcbd49f4c3887c288c30
easttiger1230 <easttiger.1230@au.com>
2023-01-30 13:36:14 +0900

    kmtんbvdbmghcfdcsぁsx

--- resources/views/matter/matterRegistar.blade.php
+++ resources/views/matter/matterRegistar.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="p-acinfo__container">
-    <div class="nes-container is-dark with-title">
+    <div class="nes-container is-dark with-title" style="margin-top:70px;">
         <p>登録完了</p>
         <a href="/company">マイページへ</a>
     </div>
