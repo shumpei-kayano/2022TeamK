7933917a9aa2700fe63b6506806920577134fb23
easttiger1230 <easttiger.1230@au.com>
2023-01-31 14:26:01 +0900

    テーブルを全てに適用させるこの後ね。

--- resources/views/userMypage/favorite.blade.php
+++ resources/views/userMypage/favorite.blade.php
@@ -7,7 +7,7 @@
 <h1 class="p-form" style="padding-top: 45px;">お気に入りリスト</h1>
 
 <div class="p-acinfo__container2">
-    <div class="nes-container is-dark with-title p-form__container2" style="overflow: scroll; max-height: 350px; border: 5px solid #fff; border-radius: 10px; padding-top:0;">
+    <div class="nes-container is-dark with-title p-form__container2" style="overflow: scroll; max-height: 350px; border: 5px solid #fff; border-radius: 10px; padding-top:0; padding-left:0; padding-right:0;">
         {{-- <a href="https://www.instagram.com/miura_koutaro?ref=badge" class="insta_btn3">
             <i class="fab fa-instagram"></i><div style="text-align: center">それでも!!!</div>
         </a> --}}
@@ -16,20 +16,21 @@
             <tr style="padding-left: 0; padding-right:0;">
                 <th>案件名</th>
                 <th>言語</th>
-                <th>エリア</th>
+                <th style="width: 40px;">エリア</th>
                 <th>特記事項</th>
-                <th class="nes-btn is-primary" style="height:35px; width:60px; text-align:center; padding-top:0px; ">詳細</th>
+                <th style="width: 30px;"></th>
+                {{-- <th class="nes-btn is-primary" style="height:35px; width:60px; text-align:center; padding-top:0px; ">詳細</th> --}}
             </tr>
             </thead>
         @foreach($favorites as $favorite)
         
                 <tbody>
                 <tr>
-                    <td class="p-show__tokki">{{ $favorite->matter_name }}</td>
+                    <td class="p-show__tokki" style="padding-left: 20px;">{{ $favorite->matter_name }}</td>
                     <td>{{ $favorite->language_name }}</td>
                     <td>{{ $favorite->prefectures_name }}</td>
                     <td class="p-show__tokki">{{ $favorite->remarks }}</td>
-                    <td><a href="{{ route('matter.detail', ['id'=>$favorite->matter_id]) }}" class="nes-btn is-primary" style="height:35px; width:60px; text-align:center; padding-top:0px;">詳細</a></td>
+                    <td><a href="{{ route('matter.detail', ['id'=>$favorite->matter_id]) }}" style="height:35px; width:60px; text-align:center; padding-top:0px; padding-right:10px; color:aqua;">詳細</a></td>
                 </tr>
                 </tbody>
         @endforeach
@@ -45,7 +46,8 @@
  th,td {
   padding: 1rem 2rem;
   text-align: center;
-  border-bottom: 1px solid #ddd;
+  border-bottom: 1px solid rgb(217, 206, 206);
+  border-color: rgb(217, 206, 206);
  }
 
  th {
@@ -53,8 +55,8 @@
   top: 0;
   font-weight: normal;
   font-size: .875rem;
-  color: #666;
-  background: #eee;
+  color:black;
+  background-color: rgb(217, 206, 206);
 }
 
 </style>
