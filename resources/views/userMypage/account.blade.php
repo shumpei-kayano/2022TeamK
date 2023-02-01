1da4185b5072d55303e139a4442b1991bd87bf7e
funyanko <ooi2172406@stu.o-hara.ac.jp>
2023-02-01 11:02:45 +0900

    装備

--- resources/views/userMypage/account.blade.php
+++ resources/views/userMypage/account.blade.php
@@ -19,7 +19,23 @@
         @endphp
         <p>次のランクまで：{{ $next }}</p>
     </div>
-    </div>
+</div>
+
+{{-- 装備（言語の追加） --}}
+    @php
+    $equipments = DB::table('portfolios')->where('user_id', $user->id)->first();
+    $equipment1 = DB::table('development_languages')->find($equipments->development_language_id1); 
+    $equipment2 = DB::table('development_language2s')->find($equipments->development_language_id2); 
+    $equipment3 = DB::table('development_language3s')->find($equipments->development_language_id3); 
+    $equipment4 = DB::table('development_language4s')->find($equipments->development_language_id4); 
+    
+    @endphp
+    E　{{$equipment1->language_name}}
+    E　{{$equipment2->language_name}}
+    E　{{$equipment3->language_name}}
+    E　{{$equipment4->language_name}}
+    
+
     <div class="p-acinfo__btn-container">
         <form action='{{ route('account_edit') }}' method="get">
             @csrf
