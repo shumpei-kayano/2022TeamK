36fb717d425057c4fc0f665e9aeb741137f07ed4
easttiger1230 <easttiger.1230@au.com>
2023-02-01 12:27:01 +0900

    愛はきっと奪うでも、与えるでもなくて、気がつけばそこにあるもの。

--- resources/views/portfolio/portfolio.blade.php
+++ resources/views/portfolio/portfolio.blade.php
@@ -3,9 +3,33 @@
 @section('title','ポートフォリオ')
 
 @section('content')
-<h1 class="p-form" style="padding-top: 30px;">ポートフォリオ</h1>
+
+<style>
+    table {
+            border-collapse: collapse;
+            width: 100%;
+            }
+ th,td {
+  padding: 1rem 2rem;
+  text-align: center;
+  border-bottom: 1px solid rgb(217, 206, 206);
+  border-color: rgb(217, 206, 206);
+ }
+
+ th {
+  position: sticky;
+  top: 0;
+  font-weight: normal;
+  font-size: .875rem;
+  color:black;
+  background-color: rgb(217, 206, 206);
+}
+
+</style>
+
+<h1 class="p-form" style="padding-top: 30px; border: 5px ">ポートフォリオ</h1>
     <div class="portfolio">
-        <div class="nes-container is-dark with-title portfolio__container">
+        <div class="nes-container is-dark with-title portfolio__container" style="solid #fff; border-radius: 10px; margin-top:-40px;">
             <a href="portfolioAdd"><button type="button" class="nes-btn is-primary portfolio__btn">ポートフォリオを<br>作成する</button></a>
             <a href="portfolioEdit"><button type="button" class="nes-btn is-warning portfolio__btn">ポートフォリオを<br>編集する</button></a>
             <a href="portfolioDel"><button type="button" class="nes-btn is-error portfolio__btn">ポートフォリオを<br>削除する</button></a>
