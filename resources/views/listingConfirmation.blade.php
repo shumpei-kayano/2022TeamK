4a2473ddec1dcaaf6f3b5c641b3538df01da7e14
miurakoutarou <koutarousanto@gmail.com>
2023-02-02 17:04:08 +0900

    中身やってる人達終わりましたぁ〜？　マダァ〜？

--- resources/views/listingConfirmation.blade.php
+++ resources/views/listingConfirmation.blade.php
@@ -4,6 +4,17 @@
 
 @section('content')
 
-<h1 class="p-form">みうらこうたろう</h1>
+<h1 class="p-form">みうらこうたろう<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html></h1>
 
 @endsection
