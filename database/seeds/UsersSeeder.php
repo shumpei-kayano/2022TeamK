b7286e5285cf0882540b3799e02eda1bd2d7f90c
funyanko <ooi2172406@stu.o-hara.ac.jp>
2023-02-08 12:12:22 +0900

    seed 変更

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -11,6 +11,27 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 10)->create();
+        DB::table('users')->insert([
+                [
+                    'name' => '大原太郎',
+                    'email' => 'o-hara@ac.jp',
+                    'email_verified_at' => now(),
+                    'password' => \Hash::make('o-hara'),
+                    'remember_token' => Str::random(10),
+                    'check' => '0',
+                ],
+        
+                [
+                    'name' => '大原花子',
+                    'email' => 'hanako@ac.jp',
+                    'email_verified_at' => now(),
+                    'password' => \Hash::make('o-hara'),
+                    'remember_token' => Str::random(10),
+                    'check' => '0',
+                ],
+                
+            ]);
+
+        factory(App\User::class, 3)->create();
     }
 }
