aaca5a8ecfd97b69f79d94105fb83cba28327802
funyanko <ooi2172406@stu.o-hara.ac.jp>
2023-02-08 12:20:49 +0900

    勝った

--- resources/views/matter_detail.blade.php
+++ resources/views/matter_detail.blade.php
@@ -124,7 +124,7 @@
       <div class="p-detail__tokki">
           <!-- 特記事項 -->
           <label for="dark_select" style="color:#fff; padding-left: 5px; padding-right:5px; margin:30px;">特記事項</label><br>
-          <p>{{ $matter->remarks }}</p><br>
+          <p style="white-space: pre-wrap;">{{ $matter->remarks }}</p><br>
           <!--<textarea name="remarks" id="" class="nes-textarea is-dark p-anken2__textarea" aria-describedby="basic-addon2" disabled>{{ $matter->remarks }}</textarea><br>-->
       </div>
 
